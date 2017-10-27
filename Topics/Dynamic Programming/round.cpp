fd3c9eba00c98564e673904e1eadb4e64c08d369
Einstein314 <konwoo.kim@gmail.com>
2017-10-26 23:20:21 -0400

    Update round.cpp

--- Topics/Dynamic Programming/round.cpp
+++ Topics/Dynamic Programming/round.cpp
@@ -1,23 +1,61 @@
+#include "bits/stdc++.h"
 
-#include<bits/stdc++.h>
 using namespace std;
-#define int long long
-int dp[234][5200],n,k,tmp;
-main(){
-	memset(dp,-1,sizeof dp);
-	dp[0][0]=0;
-	cin>>n>>k;
-	for(int s=0;s<n;s++){
-		cin>>tmp;
-		int t=0,f=0;
-		while(tmp%2==0) t++,tmp/=2;
-		while(tmp%5==0) f++,tmp/=5;
-		for(int i=k-1;i>=0;i--)
-			for(int j=0;j<5200;j++)
-				if(dp[i][j]!=-1)
-					dp[i+1][j+f]=max(dp[i+1][j+f],dp[i][j]+t);
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+#define FOR(i, a, b) for (int i=a; i<b; i++)
+#define F0R(i, a) for (int i=0; i<a; i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+const int MOD = 1000000007;
+double PI = 4*atan(1);
+
+int dp[205][6000], n, k;
+ll temp;
+int main()
+{
+
+	cin >> n >> k;
+	memset(dp, -1, sizeof(dp));
+    dp[0][0] = 0;
+    
+	F0R(i, n){
+		cin >> temp;
+		int t = 0;
+		int f = 0;
+
+		while (temp % 5 == 0) temp/=5, f++;
+		while (temp % 2 == 0) temp/=2, t++;
+
+		F0Rd(j, k){
+			F0R(l, 6000){
+				if (dp[j][l] != -1){
+					dp[j+1][l+f] = max(dp[j+1][l+f], dp[j][l] + t);
+				}
+			}
+		}
 	}
-	int ans=0;
-	for(int i=0;i<5200;i++) ans=max(ans,min(i,dp[k][i]));
-	cout<<ans<<endl;
+
+	int ans = 0;
+	F0R(i, 6000){
+		ans = max(ans, min(i, dp[k][i]));
+	}
+
+	cout << ans;
+   
+
+
+
+    return 0;
 }
