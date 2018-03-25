a201a754a2c47259a9d7d7f0d0ff468c7ce0241f
Einstein314 <konwoo.kim@gmail.com>
2018-03-25 18:04:06 -0400

    Update closing.cpp

--- Division Specific/Silver/closing.cpp
+++ Division Specific/Silver/closing.cpp
@@ -1,20 +1,15 @@
-#include<fstream>
-#include<set>
-#include<map>
-#include<unordered_map>
-#include<cmath>
-#include<cstring>
-#include<string>
-#include<bitset>
-#include<algorithm>
-#include<vector>
-#include <queue>
+//#include "/Users/konwoo/stdc++.h"
+#include "bits/stdc++.h"
 
 using namespace std;
+
+
 
 typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> pii;
+typedef vector<pii> vii;
+
 
 #define FOR(i, a, b) for (int i=a; i<b; i++)
 #define F0R(i, a) for (int i=0; i<a; i++)
@@ -28,45 +23,92 @@ typedef pair<int, int> pii;
 #define lb lower_bound
 #define ub upper_bound
 
-set<int> adj[3001];
+const int MOD = 1000000007;
+double PI = 4*atan(1);
 
-int N,M,visit[3001];
-vi permute;
+int N, M;
 
-void rem(int vert) {
-    for (int a: adj[vert]) adj[a].erase(vert);
-}
+int order[3010];
+//vector< vector< pii > > AdjList(3010, vector<pii>(3010, make_pair(-1, 0)));
+vector <vector< pii > > AdjList(3010);
+vi dfs_num;
 
-int get(int vert) {
-    queue<int> todo;
-    todo.push(vert);
-    F0R(i,3001) visit[i] = 0;
-    int cur = 0;
-    while (todo.size()) {
-        int a = todo.front();
-        todo.pop();
-        visit[a] = 1; cur++;
-        for (int b: adj[a]) if (visit[b] == 0) {
-            visit[b] = -1;
-            todo.push(b);
+void dfs(int u, int ord){
+    dfs_num[u] = 1;
+    F0R(j, (int)AdjList[u].size()){
+        pii v = AdjList[u][j];
+        if (dfs_num[v.first] == 0 && v.s > ord){
+            dfs(v.first, ord);
         }
     }
-    return cur;
 }
 
-int main() {
-    ifstream cin ("closing.in");
-    ofstream cout ("closing.out");
-    cin >> N >> M; permute.resize(N);
-    F0R(i,M) {
-        pii x; cin >> x.f >> x.s;
-        adj[x.f].insert(x.s);
-        adj[x.s].insert(x.f);
+int CC(int o){
+    int numCC = 0;
+    dfs_num.assign(N, 0);
+    F0R(i, N){
+        if (dfs_num[i] == 0 && order[i] > o){
+            ++numCC;
+            dfs(i, o);
+        }
     }
-    F0R(i,N) cin >> permute[i];
-    F0R(i,N) {
-        if (get(permute[N-1]) == N-i) cout << "YES\n";
-        else cout << "NO\n";
-        rem(permute[i]);
-    }
+    return numCC;
 }
+
+
+
+
+
+int main(){
+//    ifstream fin("/Users/konwoo/input.txt");
+//    ofstream fout("/Users/konwoo/output.txt");
+    ifstream fin("closing.in");
+    ofstream fout("closing.out");
+
+    fin >> N >> M;
+
+    F0R(i, M){
+        int a, b;
+        fin >> a >> b;
+        AdjList[a-1].pb(mp(b-1, 0));
+        AdjList[b-1].pb(mp(a-1, 0));
+    }
+
+    F0R(i, N){
+        int c;
+        fin >> c;
+        order[c - 1] = i+1;
+    }
+
+    F0R(i, N){
+        F0R(j, (int)AdjList[i].size()){
+            int a = min(order[i], order[AdjList[i][j].f]);
+            AdjList[i][j].s = a;
+            //fout << i << " " << AdjList[i][j].f << " " << AdjList[i][j].s << "\n";
+        }
+    }
+
+
+    F0R(i, N){
+        if (CC(i) == 1){
+            fout << "YES" << "\n";
+        }
+        else{
+            fout << "NO" << "\n";
+        }
+    }
+
+
+
+
+
+
+
+
+    return 0;
+}
+
+
+
+
+
