a11887bc327f0f54c12d72941513a8dfabdd36f1
konwook <konwoo.kim@gmail.com>
2020-05-18 01:45:13 -0400

    added BIT

---
+++ Implementations/Data Structures/BIT.cpp
@@ -0,0 +1,35 @@
+//1-indexed
+int BIT[MX];
+
+//sum[1,k]
+int query(int k){
+    int sum = 0;
+    for(;k >0;k -= k & -k) sum += BIT[k];
+    return sum;
+}
+
+//arr[k] += x
+void update(int k, int x){
+    for(;k<= n; k+= k & -k) BIT[k] += x;
+}
+
+//sum[l,r]
+int rquery(int l, int r){
+    return BIT[r] - BIT[l-1];
+}
+
+
+//find ind(th) largest element using BIT as ordered set
+int findind(int ind){
+    int lo = 0, hi = n;
+    while(lo < hi){
+        int mid = (lo + hi)/2;
+        if (query(mid) >= ind){
+            hi = mid;
+        }
+        else{
+            lo = mid + 1;
+        }
+    }
+    return lo;
+}
